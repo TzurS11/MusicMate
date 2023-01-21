7c09ef1ca0df953dca03639b7b4d64a7adb12fe1
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2023-01-21 14:51:25 +0200

    bottom navigation menu complete

--- app/src/main/java/com/example/musicmate/afterlogin.java
+++ app/src/main/java/com/example/musicmate/afterlogin.java
@@ -2,22 +2,60 @@ package com.example.musicmate;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.musicmate.databinding.ActivityAfterloginBinding;
+import com.example.musicmate.databinding.ActivityMainBinding;
+
 public class afterlogin extends AppCompatActivity implements View.OnClickListener {
     ActionBar actionBar;
+    ActivityAfterloginBinding binding;
+    String currentScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_afterlogin);
+        binding = ActivityAfterloginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new SearchFrag());
+
 
         actionBar = getSupportActionBar();
         actionBar.hide();
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.search:
+                    currentScreen = "search";
+                    replaceFragment(new SearchFrag());
+                    break;
+                case R.id.playlists:
+                    currentScreen = "playlists";
+                    replaceFragment(new PlaylistsFrag());
+                    break;
+                case R.id.settings:
+                    currentScreen = "settings";
+                    replaceFragment(new SettingsFrag());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment frag){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,frag);
+        fragmentTransaction.commit();
     }
 
     @Override
